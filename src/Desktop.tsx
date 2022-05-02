92e8d6b25effda30de17bbba3b33f94f3b555039
João Vitor Gaspar <joao.vgasparr@gmail.com>
2022-05-01 21:33:46 -0300

    ci: ci workflows

--- src/Desktop.tsx
+++ src/Desktop.tsx
@@ -1,3 +1,26 @@
-export function Desktop() {
-  return <h1>Desktop</h1>;
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Desktop() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Netlify Actions!
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default Desktop;
--- src/File.tsx
+++ src/File.tsx
@@ -1,3 +1,26 @@
-export function File() {
-  return <h1>File</h1>;
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function File() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Netlify Actions!
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default File;
